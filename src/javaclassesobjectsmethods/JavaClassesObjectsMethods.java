900104b0c334e555943c408757c800bd2b3e904b
Anna Weisbrodt <aweisbro@gmail.com>
2016-11-09 08:56:32 -0700

    classes and objects

---
+++ src/javaclassesobjectsmethods/JavaClassesObjectsMethods.java
@@ -0,0 +1,17 @@
+package javaclassesobjectsmethods;
+
+public class JavaClassesObjectsMethods {
+
+    public static void main(String[] args) {
+        Person person1 = new Person();
+        person1.name = "Joe";
+        person1.age = 20;
+        person1.sayHello();
+        person1.speak();
+
+        Person person2 = new Person();
+        person2.name = "Jane";
+        person2.age  = 60;
+        person2.speak();
+    }    
+}
---
+++ src/javaclassesobjectsmethods/Person.java
@@ -0,0 +1,14 @@
+package javaclassesobjectsmethods;
+
+class Person {
+    String name;
+    int age;
+    
+    void speak() {
+        System.out.println("My name is " + name + " and I am " + age + " years old.");
+    }
+    
+    void sayHello() {
+        System.out.println("Hello");
+    }
+}
