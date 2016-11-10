47161b95058d1451229d756d1b5faeb9cf0f75f2
Anna Weisbrodt <aweisbro@gmail.com>
2016-11-10 09:37:10 -0700

    method params

--- src/javaclassesobjectsmethods/JavaClassesObjectsMethods.java
+++ src/javaclassesobjectsmethods/JavaClassesObjectsMethods.java
@@ -6,23 +6,21 @@ public class JavaClassesObjectsMethods {
         Person person1 = new Person();
         person1.name = "Joe";
         person1.age = 20;
-        person1.sayHello();
-        person1.speak();
-
-        Person person2 = new Person();
-        person2.name = "Jane";
-        person2.age  = 60;
-        person2.speak();
-        
-        int years = person2.calculateYearsToRetirement();
-        
-        System.out.println("Years until retirement: " + years + ".");
-        
+       
         int age = person1.getAge();
         String name = person1.getName();
         
         System.out.println("Name is: " + name);
         System.out.println("Age is: " + age);
+        
+        Robot sam = new Robot();
+        
+        String greeting = "Hi I'm Sam";
+        sam.speak(greeting);
+        
+        int value = 14;
+        sam.jump(value);
+        sam.move("West", 12.2);
         
     }    
 }
