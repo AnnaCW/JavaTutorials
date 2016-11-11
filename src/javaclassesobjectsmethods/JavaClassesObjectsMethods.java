6c0c56be5f17d16ff83e1845c233f391e7311878
Anna Weisbrodt <aweisbro@gmail.com>
2016-11-11 11:42:08 -0700

    string builder and string formatting

--- src/javaclassesobjectsmethods/JavaClassesObjectsMethods.java
+++ src/javaclassesobjectsmethods/JavaClassesObjectsMethods.java
@@ -3,6 +3,30 @@ package javaclassesobjectsmethods;
 public class JavaClassesObjectsMethods {
     
     public static void main(String[] args) {
+        
+       StringBuilder sb = new StringBuilder();
+       
+       sb.append("My name is Bob");
+       sb.append(" ").append("I am a lion tamer");
+       
+       System.out.println(sb);
+      
+        // more efficient than just creating a string
+        //string is immutable, so what appears to be changing string
+        //is actually creating a new string and reassigning var
+
+        //*** STRING FORMATTING ********
+
+        System.out.println("This is some text.\tThat was a tab.\nThat was a new line.");
+        
+        for (int i = 0; i < 12; i++) {
+            System.out.printf("%2d: some text here\n", i);
+
+        }
+        
+        System.out.printf("Total value: %.2f\n", 5.6);
+        
+//*******************
        Thing.description = "I am a thing";
        
        Thing.showInfo();
