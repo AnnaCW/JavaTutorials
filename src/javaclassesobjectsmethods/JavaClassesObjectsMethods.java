94b25d4c85278a84f3473d2e2d048181824d9888
Anna Weisbrodt <aweisbro@gmail.com>
2016-11-12 12:00:34 -0700

    casting numerical values

--- src/javaclassesobjectsmethods/JavaClassesObjectsMethods.java
+++ src/javaclassesobjectsmethods/JavaClassesObjectsMethods.java
@@ -1,15 +1,43 @@
 package javaclassesobjectsmethods;
 
-//ENCAPSULATION
+//CASTING NUMERICAL VALUES
 
 public class JavaClassesObjectsMethods {
     
     public static void main(String[] args) {
-       Plant2 plant = new Plant2();
-       
-       plant.setName("Greeny");
-       System.out.println(plant.getName());
-       System.out.println(plant.getData());
+        
+        byte byteValue = 20;
+        short shortValue = 55; 
+        
+        int intValue = 888;
+        System.out.println("intValue: " + intValue);
+        
+        long longValue = 23333;
+        System.out.println("longValue: " + longValue);
+        
+        float floatValue = 8334.8f;
+        System.out.println("floatValue: " + floatValue);
+
+        double doubleValue = 32.4;
+        System.out.println("doubleValue: " + doubleValue);
+
+               
+        intValue = (int)longValue;
+//            type I'm casting to (int), then value I'm casting to
+        System.out.println("long to int: " + intValue);
+        
+        doubleValue = intValue;
+//        no casting needed because no potential problem here
+        System.out.println("int to double: " + doubleValue);
+        
+        intValue = (int)floatValue;
+        System.out.println("float to int: " + intValue);
+        
+        System.out.println("byte max: " + Byte.MAX_VALUE);
+
+        byteValue = (byte)128;
+        System.out.println("128 to byte: " + byteValue);
+//        unexpected behavior - 128 is too big 
     }
       
 }
