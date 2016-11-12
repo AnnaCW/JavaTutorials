e0a510ffbf8d8300aaa8c491d630ea62aa08db9d
Anna Weisbrodt <aweisbro@gmail.com>
2016-11-12 09:15:25 -0700

    toString()

--- src/javaclassesobjectsmethods/JavaClassesObjectsMethods.java
+++ src/javaclassesobjectsmethods/JavaClassesObjectsMethods.java
@@ -4,47 +4,10 @@ public class JavaClassesObjectsMethods {
     
     public static void main(String[] args) {
         
-       StringBuilder sb = new StringBuilder();
-       
-       sb.append("My name is Bob");
-       sb.append(" ").append("I am a lion tamer");
-       
-       System.out.println(sb);
-      
-        // more efficient than just creating a string
-        //string is immutable, so what appears to be changing string
-        //is actually creating a new string and reassigning var
+       Toad toad1 = new Toad(7, "Todd");
+       Toad toad2 = new Toad(5, "Tad");
 
-        //*** STRING FORMATTING ********
-
-        System.out.println("This is some text.\tThat was a tab.\nThat was a new line.");
-        
-        for (int i = 0; i < 12; i++) {
-            System.out.printf("%2d: some text here\n", i);
-
-        }
-        
-        System.out.printf("Total value: %.2f\n", 5.6);
-        
-//*******************
-       Thing.description = "I am a thing";
-       
-       Thing.showInfo();
-       
-       System.out.println("Before creating objects, count is: " + Thing.count);
-       
-       Thing thing1 = new Thing();
-       Thing thing2 = new Thing();
-       
-       thing1.name = "Bob";
-       thing2.name = "Sue";
-       
-       thing1.showName();
-       thing2.showName();
-       
-       System.out.println(Thing.LUCKY_NUMBER);
-       
-       System.out.println("After creating objects, count is: " + Thing.count);
-
+       System.out.println(toad1); 
+       System.out.println(toad2);
     }
 }
---
+++ src/javaclassesobjectsmethods/Toad.java
@@ -0,0 +1,26 @@
+
+package javaclassesobjectsmethods;
+
+ class Toad {
+    private int id; 
+
+    private String name;
+    
+    
+    public Toad(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+    
+//    public String toString() {
+//        StringBuilder sb = new StringBuilder();
+//        sb.append(id).append(": ").append(name);
+//        return sb.toString();
+//    }
+    
+//    same result as the above: 
+    
+//    public String toString() {
+//        return String.format("%4d: %s", id, name);
+//    }
+}
