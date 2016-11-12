f01d3e73eca7a18875b05993b85e5bd9f6018cd2
Anna Weisbrodt <aweisbro@gmail.com>
2016-11-12 10:02:04 -0700

    packages

--- src/javaclassesobjectsmethods/JavaClassesObjectsMethods.java
+++ src/javaclassesobjectsmethods/JavaClassesObjectsMethods.java
@@ -1,14 +1,17 @@
 package javaclassesobjectsmethods;
 //import ocean.Fish;
 //import ocean.Seaweed;
-import ocean.plants.Seaweed;
 import ocean.*;
+
+import ocean.plants.Seaweed;
+import ocean.plants.Algae;
 
 public class JavaClassesObjectsMethods {
     
     public static void main(String[] args) {
        Fish fish = new Fish();
        Seaweed sw = new Seaweed();
+       Algae ag = new Algae();
        
     }
 }
