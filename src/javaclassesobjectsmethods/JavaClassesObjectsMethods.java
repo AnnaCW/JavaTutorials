d14ea5952864f23041b74b92660dd2989b84d4e3
Anna Weisbrodt <aweisbro@gmail.com>
2016-11-12 16:02:14 -0700

    generics and wildcards

--- src/javaclassesobjectsmethods/JavaClassesObjectsMethods.java
+++ src/javaclassesobjectsmethods/JavaClassesObjectsMethods.java
@@ -1,33 +1,69 @@
 package javaclassesobjectsmethods;
 
-//USING GENERIC CLASSES - ArrayList
-
 import java.util.ArrayList;
+
+class Dog {
+    @Override
+    public String toString() {
+        return "I am a dog";
+    }
+}
+
+class Collie extends Dog {
+    @Override
+    public String toString() {
+        return "I am a collie";
+    }
+}
 
 public class JavaClassesObjectsMethods {
     
     public static void main(String[] args) {
- 
-//  MODERN STYLE
-        ArrayList<String> strings = new ArrayList<>();
+        ArrayList<Dog> list1 = new ArrayList<>();
         
-        strings.add("cat");
-        strings.add("dog");
-        strings.add("bird");
+        list1.add(new Dog());
+        list1.add(new Dog());
+        showList(list1);
         
-        String animal = strings.get(1);
+        ArrayList<Collie> list2 = new ArrayList<>();
         
-        System.out.println(strings);
-        System.out.println(animal);
-                
-//  BEFORE JAVA ~5 (introduced generics)
-       ArrayList oldList = new ArrayList();
+        list2.add(new Collie());
+        list2.add(new Collie());
+        showList(list2);
+    }
+    
+    // WILD CARD WITH UPPER BOUND
+    //java just knows its dealing with some kind of dog
+    
+     public static void showList(ArrayList<? extends Dog> list) {
+         for (Dog value: list) {
+             System.out.println(value);
+             //can call Dog methods here but not Collie methods
+         }
+    
+//         *************** WILD CARD WITH LOWER BOUND *******
+         
+//      less useful 
+//      objects here dont necessarily have Collie methods
+//      takes Collie or super class of Collie
+    
+//    public static void showList(ArrayList<? super Collie> list) {
+//        for (Object value: list) {
+//            System.out.println(value);
+//        }
+//    }
+            
+           
+//         ****************WILD CARD *****
+         
+//    public static void showList(ArrayList<?> list) {
+//         for(Object value: list) {
+//            System.out.println(value);
+//        }
+//        the question mark allows us to pass any type of array list
+//        array list consisting of any object types
+//        downside: no dog specific methods or collie-specific methods
        
-       oldList.add("apple");
-       oldList.add("banana");
-       
-       String fruit = (String)oldList.get(1);
-       System.out.println(fruit);
     }
       
 }
