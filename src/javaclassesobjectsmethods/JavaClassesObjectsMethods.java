3c366ea242252b57526a937ea3896f70cc807562
Anna Weisbrodt <aweisbro@gmail.com>
2016-11-13 10:00:28 -0700

    file reader and buffered reader (older way)

--- src/javaclassesobjectsmethods/JavaClassesObjectsMethods.java
+++ src/javaclassesobjectsmethods/JavaClassesObjectsMethods.java
@@ -1,21 +1,47 @@
 package javaclassesobjectsmethods;
 
-//ABSTRACT CLASSES
+//READING FILES WITH FILE READER / BUFFERED READER
+//this is old way; better ways introduced starting w/ Java 7
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class JavaClassesObjectsMethods {
     
     public static void main(String[] args) {
-        Motorcycle moto1 = new Motorcycle();
-        moto1.setId(5);
-        moto1.start();
         
-        Truck truck1 = new Truck();
-        truck1.setId(2);
-        truck1.start();
+       File file = new File("example.txt");
+       BufferedReader br = null;
+
+        try {
+            FileReader fr = new FileReader(file);
+            br = new BufferedReader(fr);
+              
+            String line;
+            
+            while( (line = br.readLine()) != null ) {
+               System.out.println(line);
+            }
+              
+            
+        } catch (FileNotFoundException ex) {
+            System.out.println("File Not Found: " + file.toString());
+        } catch (IOException ex) {
+            System.out.println("Cannot Read File: " + file.toString());
+        }
         
-//        Vehicle vehicle = new Vehicle();
-//        can decide that there is no use for a vehicle
-//        prevent instantiation by making it abstract
-    }
-    
+        finally {
+          try {
+                br.close();
+            } catch (IOException ex) {
+                System.out.println("Unable to close file: " + file.toString());
+            } catch(NullPointerException ex) {
+                // file was probably never opened           
+            }
+        }
+          // finally block will be executed even if exceptions are thrown          
+    }  
 }
