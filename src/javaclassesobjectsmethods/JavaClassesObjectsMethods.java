2ac5f1ca31be05aa1c7e4bdd37cdcdefd0069761
Anna Weisbrodt <aweisbro@gmail.com>
2016-11-13 08:23:21 -0700

    handling exceptions - basics

--- src/javaclassesobjectsmethods/JavaClassesObjectsMethods.java
+++ src/javaclassesobjectsmethods/JavaClassesObjectsMethods.java
@@ -1,37 +1,59 @@
 package javaclassesobjectsmethods;
 
-//READING TEXT FILES
+//HANDLING EXCEPTIONS - BASICS
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
-
+import java.io.FileReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class JavaClassesObjectsMethods {
     
-    public static void main(String[] args) throws FileNotFoundException {
-//        String fileName = "/Users/ACW/Desktop/example.txt";
-        String fileName = "example.txt";
+// THIRD WAY TO HANDLE EXCEPTIONS 
+// here we throw the error in openFile, catch it in main
 
-        File textFile = new File(fileName);
-        
-        Scanner in = new Scanner(textFile);
-        
-        int value = in.nextInt();
-        System.out.println("Read value: " + value);
-        
-        in.nextLine();
-        
-        int count = 2;
-        
-        while(in.hasNextLine()) {
-            String line = in.nextLine();
-            
-            System.out.println(count + ": " + line);
-            count++;
+    
+    public static void main(String[] args) {
+        try {
+            openFile();
+        } catch (FileNotFoundException ex) {
+           System.out.println("Could not open file");
         }
-        
-        in.close();
     }
+    
+    public static void openFile() throws FileNotFoundException {
+        File file = new File("text.txt");
+        
+        FileReader fr = new FileReader(file);
+    }
+
+    
+// SECOND WAY TO HANDLE EXCEPTIONS : try/catch block 
+//    not throwing out of main    
+//    providing code that will run if an exception is found
+    
+//    public static void main(String[] args) {
+//        File file = new File("test.txt");
+//        
+//        try {
+//            FileReader fr = new FileReader(file);            
+//        } catch (FileNotFoundException ex) {
+//            Logger.getLogger(JavaClassesObjectsMethods.class.getName()).log(Level.SEVERE, null, ex);
+//            //prints stack trace by default but can put anything here
+//        }
+//        
+//        System.out.println("Finished");
+//        // this will run even if an exception is caught
+//    }
+    
+//FIRST WAY TO HANDLE EXCEPTIONS: throw file not found out of main
+    
+//    public static void main(String[] args) throws FileNotFoundException {
+//        File file = new File("text.txt");
+//        
+//        FileReader fr = new FileReader(file);
+//    }
 }
 
+//exceptions are objects derived from the exception class
