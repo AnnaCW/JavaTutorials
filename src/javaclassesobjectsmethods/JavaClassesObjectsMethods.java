df01a2577e1640f2a2a0af6216bd80c9d6b8baba
Anna Weisbrodt <aweisbro@gmail.com>
2016-11-14 10:26:00 -0700

    try with resources

--- src/javaclassesobjectsmethods/JavaClassesObjectsMethods.java
+++ src/javaclassesobjectsmethods/JavaClassesObjectsMethods.java
@@ -1,47 +1,30 @@
 package javaclassesobjectsmethods;
 
-//READING FILES WITH FILE READER / BUFFERED READER
-//this is old way; better ways introduced starting w/ Java 7
+//TRY - WITH RESOURCES
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+   
 
 public class JavaClassesObjectsMethods {
     
     public static void main(String[] args) {
         
-       File file = new File("example.txt");
-       BufferedReader br = null;
-
-        try {
-            FileReader fr = new FileReader(file);
-            br = new BufferedReader(fr);
-              
-            String line;
-            
-            while( (line = br.readLine()) != null ) {
-               System.out.println(line);
-            }
-              
+        File file = new File("nosuchfile.txt");
+                
+        try (BufferedReader br = new BufferedReader(new FileReader(file)) ) {
             
         } catch (FileNotFoundException ex) {
-            System.out.println("File Not Found: " + file.toString());
+            System.out.println("Can't find file " + file.toString());
         } catch (IOException ex) {
-            System.out.println("Cannot Read File: " + file.toString());
+            System.out.println("Unable to read file " + file.toString());
         }
-        
-        finally {
-          try {
-                br.close();
-            } catch (IOException ex) {
-                System.out.println("Unable to close file: " + file.toString());
-            } catch(NullPointerException ex) {
-                // file was probably never opened           
-            }
-        }
-          // finally block will be executed even if exceptions are thrown          
+                
     }  
 }
+
+//try with resources was introduced in java 7
+
