3f8ac180e7bc71d53a41add8f14c9bb3932e3a31
Anna Weisbrodt <aweisbro@gmail.com>
2016-11-22 06:46:14 -0700

    ArrayList

--- src/javaclassesobjectsmethods/JavaClassesObjectsMethods.java
+++ src/javaclassesobjectsmethods/JavaClassesObjectsMethods.java
@@ -1,68 +1,48 @@
 package javaclassesobjectsmethods;
 
-// PASSING BY VALUE
+// ARRAY LIST
+
+import java.util.ArrayList;
 
 public class JavaClassesObjectsMethods {
     
     public static void main(String[] args) {
-        JavaClassesObjectsMethods app = new JavaClassesObjectsMethods();
- 
-// ***************************************************
+        ArrayList<Integer> nums = new ArrayList<>();
+        
+//      ADDING ITEMS       
+        nums.add(10);
+        nums.add(100);
+        nums.add(50);
+        
+//      RETRIEVING
+        System.out.println(nums.get(0));
+        
+        
+//      INDEXED FOR LOOP ITERATION 
+        System.out.println("\nIteration #1: ");
 
-        int value = 7;
-        System.out.println("1. Value is " + value);
+        for (int i = 0; i < nums.size(); i++) {
+            System.out.println(nums.get(i));
+        }
+        
+//      ANOTHER METHOD OF ITERATION   
+        System.out.println("\nIteration #2: ");
+        
+        for (Integer value: nums) {
+            System.out.println(value);
+        }
+        
+//     REMOVING ITEMS (BE CAREFUL) 
+        nums.remove(nums.size() - 1);
 
-        app.show(value);
-        System.out.println("4. Value is " + value);
-//        value in this scope is still 7
+        nums.remove(0);
+//        this is very slow - copies all remaining items to move them up
+//        use linked list instead
+        
+        System.out.println("\nAfter removing first and last: ");
 
-// ***************************************************
-        
-        System.out.println();
-        
-        Person person = new Person("Bob");
-        System.out.println("1. Person is: " + person);
-        
-        app.show(person);
-        
-        System.out.println("4. Person is: " + person);
-
-        
+        for (Integer value: nums) {
+            System.out.println(value);
+        }
     }
-    
-    public void show(int value) {
-        System.out.println("2. Value is " + value);
-        
-        value = 8;
-//        changing variable value only in this scope
-        
-        System.out.println("3. Value is " + value);
-
-    }
-    
-    public void show(Person person) {
-        System.out.println("2. Person is: " + person);
-        
-        person.setName("Sue");
-        
-        person = new Person("Mike");
-        
-        System.out.println("3. Person is: " + person);
-
-    }
-    
-//    method overloading: methods can have same name as long as
-//      they have different argument types
 }
-
-// OUTPUT:
-
-//1. Value is 7
-//2. Value is 7
-//3. Value is 8
-//4. Value is 7
-//
-//1. Person is: Person [name=Bob]
-//2. Person is: Person [name=Bob]
-//3. Person is: Person [name=Mike]
-//4. Person is: Person [name=Sue]
