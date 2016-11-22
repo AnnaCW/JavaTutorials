19597b4080276c364cdf75d1848f4e29538c549d
Anna Weisbrodt <aweisbro@gmail.com>
2016-11-22 07:29:45 -0700

    linked lists

--- src/javaclassesobjectsmethods/JavaClassesObjectsMethods.java
+++ src/javaclassesobjectsmethods/JavaClassesObjectsMethods.java
@@ -1,48 +1,62 @@
 package javaclassesobjectsmethods;
 
-// ARRAY LIST
+// LINKED LISTS
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 
 public class JavaClassesObjectsMethods {
     
     public static void main(String[] args) {
-        ArrayList<Integer> nums = new ArrayList<>();
+        ArrayList<Integer> al = new ArrayList<>();
         
-//      ADDING ITEMS       
-        nums.add(10);
-        nums.add(100);
-        nums.add(50);
+        LinkedList<Integer> ll = new LinkedList<>();
         
-//      RETRIEVING
-        System.out.println(nums.get(0));
-        
-        
-//      INDEXED FOR LOOP ITERATION 
-        System.out.println("\nIteration #1: ");
+        doTimings("ArrayList", al);
+        doTimings("LinkedList", ll);
+    }
+    
 
-        for (int i = 0; i < nums.size(); i++) {
-            System.out.println(nums.get(i));
-        }
-        
-//      ANOTHER METHOD OF ITERATION   
-        System.out.println("\nIteration #2: ");
-        
-        for (Integer value: nums) {
-            System.out.println(value);
-        }
-        
-//     REMOVING ITEMS (BE CAREFUL) 
-        nums.remove(nums.size() - 1);
+    private static void doTimings(String type, List<Integer> list) {
+   //  this list interface can be any kind of list
+       
+       for(int i=0; i<1E5; i++) {
+           list.add(i);
+       }
+       
+       long start = System.currentTimeMillis();
 
-        nums.remove(0);
-//        this is very slow - copies all remaining items to move them up
-//        use linked list instead
-        
-        System.out.println("\nAfter removing first and last: ");
+// add to end
+       for(int i=0; i<1E5; i++) {
+         list.add(i);
+       }      
+       
+// add to beginning
+//      for(int i=0; i<1E5; i++) {
+//         list.add(0, i);
+//       }
+       
+       long end = System.currentTimeMillis();
 
-        for (Integer value: nums) {
-            System.out.println(value);
-        }
+       System.out.println("Time taken: " + (end - start) + " ms for " + type);
     }
 }
+
+// ArrayList is faster adding to / removing from end
+// also faster retrieving an item from a particular index number
+
+// LinkedList is faster for manipulating beginning or middle of list
+
+//   Use ArrayList UNLESS you want to 
+//   add to / remove from from the beginning or middle of the list
+
+
+//Output adding to end: 
+//Time taken: 10 ms for ArrayList
+//Time taken: 17 ms for LinkedList
+
+//Output adding to beginning: 
+//Time taken: 6568 ms for ArrayList
+//Time taken: 11 ms for LinkedList
