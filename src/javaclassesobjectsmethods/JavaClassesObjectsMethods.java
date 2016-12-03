1efc042394802b33d03a352cc8563f1a6cd86f0e
Anna Weisbrodt <aweisbro@gmail.com>
2016-12-03 15:08:22 -0800

    sorted maps

--- src/javaclassesobjectsmethods/JavaClassesObjectsMethods.java
+++ src/javaclassesobjectsmethods/JavaClassesObjectsMethods.java
@@ -1,36 +1,39 @@
 package javaclassesobjectsmethods;
 
-// HASH MAP
+// SORTED MAPS
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 
 public class JavaClassesObjectsMethods {
     
     public static void main(String[] args) {
-        HashMap<Integer, String> map = new HashMap<>();
+        HashMap<Integer, String> hashMap = new HashMap<>();
+        LinkedHashMap<Integer, String> linkedHashMap = new LinkedHashMap<>();
+        TreeMap<Integer, String> treeMap = new TreeMap<>();
         
-        map.put(5, "Five");
-        map.put(6, "Six");
-        map.put(2, "Two");
-        map.put(1, "One");
+        testMap(hashMap);
+        testMap(linkedHashMap); 
+            // maintains order that items were put in 
+        testMap(treeMap);
+            // orders keys "natural order"
+    }
+    
+    public static void testMap(Map<Integer, String> map) {
+        map.put(9, "fox");
+        map.put(8, "eel");
+        map.put(4, "dog");
+        map.put(0, "mosquito");
+        map.put(16, "swan");
         
-//      map.put(6, "Hello");
-//      can't have duplicate keys
-//      this overwrites the previous 6
-
-        String text = map.get(6);
-        
-        System.out.println(text);
-        
-        for(Map.Entry<Integer, String> entry: map.entrySet()) {
-            int key = entry.getKey();
-            String value = entry.getValue();
+        for (Integer key: map.keySet()) {
+            String value = map.get(key);
             
-            System.out.println(key + ": " + value);
+            System.out.println(key + ": " + value);     
         }
-
     }
     
 }
