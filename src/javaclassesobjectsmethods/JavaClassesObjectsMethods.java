a7cb46b8406521e4d91b9ec0977b1eabd6c841df
Anna Weisbrodt <aweisbro@gmail.com>
2016-12-03 14:52:05 -0800

    hash map

--- src/javaclassesobjectsmethods/JavaClassesObjectsMethods.java
+++ src/javaclassesobjectsmethods/JavaClassesObjectsMethods.java
@@ -1,62 +1,36 @@
 package javaclassesobjectsmethods;
 
-// LINKED LISTS
+// HASH MAP
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class JavaClassesObjectsMethods {
     
     public static void main(String[] args) {
-        ArrayList<Integer> al = new ArrayList<>();
+        HashMap<Integer, String> map = new HashMap<>();
         
-        LinkedList<Integer> ll = new LinkedList<>();
+        map.put(5, "Five");
+        map.put(6, "Six");
+        map.put(2, "Two");
+        map.put(1, "One");
         
-        doTimings("ArrayList", al);
-        doTimings("LinkedList", ll);
+//      map.put(6, "Hello");
+//      can't have duplicate keys
+//      this overwrites the previous 6
+
+        String text = map.get(6);
+        
+        System.out.println(text);
+        
+        for(Map.Entry<Integer, String> entry: map.entrySet()) {
+            int key = entry.getKey();
+            String value = entry.getValue();
+            
+            System.out.println(key + ": " + value);
+        }
+
     }
     
-
-    private static void doTimings(String type, List<Integer> list) {
-   //  this list interface can be any kind of list
-       
-       for(int i=0; i<1E5; i++) {
-           list.add(i);
-       }
-       
-       long start = System.currentTimeMillis();
-
-// add to end
-       for(int i=0; i<1E5; i++) {
-         list.add(i);
-       }      
-       
-// add to beginning
-//      for(int i=0; i<1E5; i++) {
-//         list.add(0, i);
-//       }
-       
-       long end = System.currentTimeMillis();
-
-       System.out.println("Time taken: " + (end - start) + " ms for " + type);
-    }
 }
-
-// ArrayList is faster adding to / removing from end
-// also faster retrieving an item from a particular index number
-
-// LinkedList is faster for manipulating beginning or middle of list
-
-//   Use ArrayList UNLESS you want to 
-//   add to / remove from from the beginning or middle of the list
-
-
-//Output adding to end: 
-//Time taken: 10 ms for ArrayList
-//Time taken: 17 ms for LinkedList
-
-//Output adding to beginning: 
-//Time taken: 6568 ms for ArrayList
-//Time taken: 11 ms for LinkedList
