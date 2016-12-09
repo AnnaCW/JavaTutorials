a18cc94bd1633659b72a8edea9f13de59a3bc59b
Anna Weisbrodt <aweisbro@gmail.com>
2016-12-08 21:14:50 -0700

    sorting lists

--- src/javaclassesobjectsmethods/JavaClassesObjectsMethods.java
+++ src/javaclassesobjectsmethods/JavaClassesObjectsMethods.java
@@ -1,85 +1,157 @@
 package javaclassesobjectsmethods;
 
-import java.util.LinkedHashMap;
-import java.util.LinkedHashSet;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
-// USING CUSTOM OBJECTS IN SETS AND AS KEYS
 
-class Person3 {
+// SORTING LISTS
+
+class Person4 {
     private int id;
     private String name;
     
-    public Person3(int id, String name) {
+    public Person4(int id, String name) {
         this.id = id;
         this.name = name;
     }
-    
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
     public String toString() {
-        return "{ID is: " + id +"; Name is: " + name + "}";
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 3;
-        hash = 97 * hash + this.id;
-        hash = 97 * hash + Objects.hashCode(this.name);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final Person3 other = (Person3) obj;
-        if (this.id != other.id) {
-            return false;
-        }
-        if (!Objects.equals(this.name, other.name)) {
-            return false;
-        }
-        return true;
+        return id + ": " + name;
     }
     
 }
 
-public class JavaClassesObjectsMethods {
-    
-    public static void main(String[] args) {
-        
-        Person3 p1 = new Person3(0, "Bob");
-        Person3 p2 = new Person3(1, "Sue");
-        Person3 p3 = new Person3(2, "Mike");
-        Person3 p4 = new Person3(1, "Sue");
+class StringLengthComparator implements Comparator<String> {
 
+    @Override
+    public int compare(String o1, String o2) {
         
-        Map<Person3, Integer> map = new LinkedHashMap<Person3, Integer>();
+        int len1 = o1.length();
+        int len2 = o2.length();
         
-        map.put(p1, 1);
-        map.put(p2, 2);
-        map.put(p3, 3);
-        map.put(p4, 1);
-        
-        for(Person3 key: map.keySet()) {
-            System.out.println(key + ": " + map.get(key));
+        if (len1 > len2) {
+            return 1;
         }
         
-        Set<Person3> set = new LinkedHashSet<Person3>();
+        else if(len1 < len2) {
+            return -1;
+        }
         
-        set.add(p1);
-        set.add(p2);
-        set.add(p3);
-        set.add(p4);
+        return 0;
+    }
+}
+
+class ReverseAlphabeticalComparator implements Comparator<String> {
+   public int compare(String o1, String o2) {
+       return -o1.compareTo(o2);
+   }
+}
+
+public class JavaClassesObjectsMethods {
+    
+    
+    public static void main(String[] args) {
+       
+///////////// Sorting Strings ///////////////////////
+       List<String> animals = new ArrayList<>();
+       
+       animals.add("cat");
+       animals.add("lion");
+       animals.add("dog");
+       animals.add("snake");
+       animals.add("rat");
+       animals.add("bat");
+
+//       Collections.sort(animals, new StringLengthComparator());
+       Collections.sort(animals, new ReverseAlphabeticalComparator());
+
+       
+       for(String animal: animals) {
+           System.out.println(animal);
+       }
+ ///////////// Sorting Numbers ///////////////////////
+      
+       List<Integer> numbers = new ArrayList<>();
+       
+       numbers.add(3);
+       numbers.add(1);
+       numbers.add(36);
+       numbers.add(16);
+       numbers.add(12);
+       
+//       Collections.sort(numbers);
+       
+       Collections.sort(numbers, new Comparator<Integer>() {
+           @Override
+           public int compare(Integer num1, Integer num2) {
+               return -num1.compareTo(num2);
+           }
+       });
+       
+        for(Integer number: numbers) {
+           System.out.println(number);
+       }
         
-        System.out.println(set);
+  ///////////// Sorting Arbitrary Objects ///////////////////////
+      
+       List<Person4> people = new ArrayList<Person4>();
+       
+       people.add(new Person4(1, "Jane"));
+       people.add(new Person4(4, "Bob"));
+       people.add(new Person4(3, "Anna"));
+       people.add(new Person4(2, "Greg"));
+       
+//       Collections.sort(people); doesnt work b/c no natural order
+
+// sort by ID 
+        Collections.sort(people, new Comparator<Person4>() {
+            public int compare (Person4 p1, Person4 p2) {
+                
+                if(p1.getId() > p2.getId()) {
+                    return 1;
+                }
+                else if(p1.getId() < p2.getId()) {
+                    return -1;
+                }
+                
+                return 0;
+            }
+        });
+        
+        for(Person4 person: people) {
+           System.out.println(person);
+        }
+        
+// sort by name
+
+        Collections.sort(people, new Comparator<Person4>() {
+            public int compare (Person4 p1, Person4 p2) {
+                return p1.getName().compareTo(p2.getName());
+            }
+        });
+       
+       for(Person4 person: people) {
+           System.out.println(person);
+       }
+
     }   
 }
